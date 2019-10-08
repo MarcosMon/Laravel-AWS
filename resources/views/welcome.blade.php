8cc683cf5e86bc33f4d58d206c7291c7279e7fa4
marc monjon <sysmarc13@hotmail.com>
2019-10-08 16:48:38 +0200

    cambio de local a servidor

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Bienvenidos al inframundo
+                    Bienvenidos!!!!!!
                 </div>
 
                 <div class="links">
