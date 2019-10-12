28c9985072c4e4b179e1e3a488a2adc2e36ef899
marc monjon <sysmarc13@hotmail.com>
2019-10-12 15:18:44 +0200

    Pruebas

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/hello', function () {
+    return 'Hello';
+});
+
 Route::get('ofertas/{nombre?}', 'pagesController@ofertas')->name('ofertas');
 
 
